1969559f1894f2c87cb8c0a83c51deebb333f01a
Nicholas Anderson <Nicholasaa@gmail.com>
2020-07-16 14:37:21 -0700

    Keep on updating

---
+++ DeplyConfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp ./Config/Arcadia.config /mnt/data/terraria/Arcadia/serverconfig.txt
+cp ./Config/Classic.config /mnt/data/terraria/Classic/serverconfig.txt
+cp ./Config/Expert.config /mnt/data/terraria/Expert/serverconfig.txt
+cp ./Config/Fresh.config /mnt/data/terraria/Fresh/serverconfig.txt
+cp ./Config/Journey.config /mnt/data/terraria/Journey/serverconfig.txt
+cp ./Config/Master.config /mnt/data/terraria/Master/serverconfig.txt
